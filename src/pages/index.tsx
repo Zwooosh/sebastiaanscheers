5331f1b15e96163396a6cd8fb639ec74a18bd5b8
Sebastiaan Scheers <ik@sebastiaanscheers.nl>
2020-06-04 15:22:29 +0200

    Clone boilerplate

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+
+export const Home = (): JSX.Element => (
+  <div className="container">
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <main>
+      <h1 className="title">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
+    </main>
+
+    <style jsx global>{`
+      html,
+      body {
+        padding: 0;
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+
+      * {
+        box-sizing: border-box;
+      }
+    `}</style>
+  </div>
+)
+
+export default Home
