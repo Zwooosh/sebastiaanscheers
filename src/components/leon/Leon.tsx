89cf0d556324faad0db367aa6202ca50ca36529a
Sebastiaan Scheers <ik@sebastiaanscheers.nl>
2020-06-06 17:44:29 +0200

    change basic animation

--- src/components/leon/Leon.tsx
+++ src/components/leon/Leon.tsx
@@ -1,18 +1,27 @@
 import React, { useRef, useEffect } from 'react'
 import { gsap } from 'gsap'
+import Router from 'next/router'
+
 import LeonSans from '../../lib/leon.js'
+
+interface IFont {
+  total: number
+  leon: typeof LeonSans[]
+  rect: { h: number; w: number }
+  gap?: { x: number; y: number }
+}
 
 const Leon = () => {
   const canvas = useRef<HTMLCanvasElement>()
   const pixelRatio = 2
-  const sizeRatio = 16
+  const defaultWeight = 200
 
   let screenWidth: number
   let screenHeight: number
   let ctx: CanvasRenderingContext2D
 
   const firstNameText = 'Sebastiaan'.split('')
-  const firstName = {
+  const firstName: IFont = {
     total: firstNameText.length,
     leon: [],
     rect: {
@@ -24,7 +33,7 @@ const Leon = () => {
       y: 0,
     },
   }
-  const lastNameText = 'Scheers'.split('')
+  const lastNameText = 'Scheers.nl'.split('')
   const lastName = {
     total: lastNameText.length,
     leon: [],
@@ -54,15 +63,18 @@ const Leon = () => {
   }
 
   const init = () => {
+    const sizeRatio = 16
+    const textColor = '#000000'
+
     ctx = canvas.current.getContext('2d')
     canvasResize()
 
     firstName.leon = firstNameText.map((l) => {
       const letter = new LeonSans({
         text: l,
-        color: ['#000000'],
+        color: [textColor],
         size: getSize(sizeRatio),
-        weight: 200,
+        weight: defaultWeight,
       })
       // letter.drawing.forEach((d) => (d.value = 0))
       return letter
@@ -71,120 +83,224 @@ const Leon = () => {
     lastName.leon = lastNameText.map((l) => {
       const letter = new LeonSans({
         text: l,
-        color: ['#000000'],
+        color: [textColor],
         size: getSize(sizeRatio),
-        weight: 200,
+        weight: defaultWeight,
       })
       // letter.drawing.forEach((d) => (d.value = 0))
       return letter
     })
   }
 
-  const draw = () => {
-    requestAnimationFrame(draw)
+  const render = () => {
+    requestAnimationFrame(render)
     ctx.clearRect(0, 0, screenWidth, screenHeight)
 
+    const letterSpacing = 5
+
+    // Tracking firstName
     let w = 0,
       h = 0
-    for (let i = 0; i < firstName.total; i++) {
-      w += firstName.leon[i].rect.w + 5
-      h = firstName.leon[i].rect.h
+    for (const l of firstName.leon) {
+      w += l.rect.w + letterSpacing
+      h = l.rect.h
     }
     firstName.rect = { w, h }
 
+    // Tracking lastName
     w = 0
     h = 0
-    for (let i = 0; i < lastName.total; i++) {
-      w += lastName.leon[i].rect.w + 5
-      h = lastName.leon[i].rect.h
+    for (const l of lastName.leon) {
+      w += l.rect.w + letterSpacing
+      h = l.rect.h
     }
     lastName.rect = { w, h }
 
-    let x = (screenWidth - firstName.rect.w) / 2
-    const y = (screenHeight - firstName.rect.h - lastName.rect.h) / 2
+    // Positioning firstName
+    let x = (screenWidth - firstName.rect.w) / 2 + firstName.gap.x
+    let y =
+      (screenHeight - firstName.rect.h - lastName.rect.h) / 2 + firstName.gap.y
     let space = 0
-
-    for (let j = 0; j < firstName.total; j++) {
-      firstName.leon[j].position(x + space, y)
-      firstName.leon[j].draw(ctx)
-      space += firstName.leon[j].rect.w + 5
+    for (const l of firstName.leon) {
+      l.position(x + space, y)
+      l.draw(ctx)
+      space += l.rect.w + letterSpacing
     }
 
-    x = (screenWidth - lastName.rect.w) / 2
+    // Positioning lastName
+    x = (screenWidth - lastName.rect.w) / 2 + lastName.gap.x
+    y = y + firstName.rect.h + lastName.gap.y
     space = 0
-    for (let j = 0; j < lastName.total; j++) {
-      lastName.leon[j].position(x + space, y + firstName.rect.h)
-      lastName.leon[j].draw(ctx)
-      space += lastName.leon[j].rect.w + 5
+    for (const l of lastName.leon) {
+      l.position(x + space, y)
+      l.draw(ctx)
+      space += l.rect.w + letterSpacing
     }
   }
 
-  const animateStroke = () => {
+  const animateDrawing = () => {
+    const duration = 2
+    const delayPerLetter = 0.3
+    const delayLastName = delayPerLetter * (firstName.total - 1)
+    const delayNextCall = 0.4
+
     for (let i = 0; i < firstName.total; i++) {
-      const letter = firstName.leon[i]
-      for (let j = 0; j < firstName.leon[i].drawing.length; j++) {
+      for (const d of firstName.leon[i].drawing) {
         gsap.fromTo(
-          letter.drawing[j],
-          2,
+          d,
           {
             value: 0,
           },
           {
-            delay: i * 0.2,
+            duration,
+            delay: i * delayPerLetter,
             value: 1,
             ease: 'power4.out',
           }
         )
       }
-      const delay = 0.3 * (firstName.total - 1)
-      for (let i = 0; i < lastName.total; i++) {
-        const letter = lastName.leon[i]
-        for (let j = 0; j < lastName.leon[i].drawing.length; j++) {
-          gsap.fromTo(
-            letter.drawing[j],
-            2,
-            {
-              value: 0,
-            },
-            {
-              delay: delay + i * 0.2,
-              value: 1,
-              ease: 'power4.out',
-            }
-          )
-        }
-      }
-      gsap.delayedCall(
-        0.3 * (firstName.total + lastName.total - 1) + 0.4,
-        animateWeight
-      )
     }
+    for (let i = 0; i < lastName.total; i++) {
+      for (const d of lastName.leon[i].drawing) {
+        gsap.fromTo(
+          d,
+          {
+            value: 0,
+          },
+          {
+            duration,
+            delay: delayLastName + i * delayPerLetter,
+            value: 1,
+            ease: 'power4.out',
+          }
+        )
+      }
+    }
+    gsap.delayedCall(
+      delayPerLetter * (firstName.total + lastName.total - 1) + delayNextCall,
+      animateWeight
+    )
   }
 
   const animateWeight = () => {
+    const weight = 600
+    const duration = 1.4
+    const delayPerLetter = 0.05
+    const delayReset = 0.7
+    const delayLastName = delayPerLetter * (firstName.total - 1)
+    const delayNextCall = 0.8
+
     for (let i = 0; i < firstName.total; i++) {
       gsap.to(firstName.leon[i], {
-        duration: 1.4,
-        delay: 0.06 * i,
-        weight: 600,
+        duration,
+        delay: delayPerLetter * i,
+        weight,
+        ease: 'power4.out',
+      })
+      gsap.to(firstName.leon[i], {
+        duration,
+        delay: delayPerLetter * i + delayReset,
+        weight: defaultWeight,
         ease: 'power4.out',
       })
     }
-    const delay = 0.06 * (firstName.total - 1)
+
     for (let i = 0; i < lastName.total; i++) {
       gsap.to(lastName.leon[i], {
-        duration: 1.4,
-        delay: delay + 0.06 * i,
-        weight: 600,
+        duration,
+        delay: delayLastName + delayPerLetter * i,
+        weight,
+        ease: 'power4.out',
+      })
+      gsap.to(lastName.leon[i], {
+        duration,
+        delay: delayLastName + delayPerLetter * i + delayReset,
+        weight: defaultWeight,
         ease: 'power4.out',
       })
     }
+    gsap.delayedCall(
+      delayPerLetter * (firstName.total + lastName.total - 1) + delayNextCall,
+      animateZoom
+    )
+  }
+  // 0,0,sw,sh
+  function a(e, t, i, n) {
+    // sw - 0
+    // sh - 0
+    var o = i - e,
+      s = n - t
+    return Math.sqrt(o * o + s * s)
+  }
+
+  const animateZoom = () => {
+    const duration = 1.8
+    const size = console.log('animateZoom -> size', size)
+    0.6 * Math.sqrt(screenWidth * screenWidth + screenHeight * screenHeight)
+    const x = -(size >> 1)
+    const y = size / 20
+
+    for (let i = 0; i < firstName.total; i++) {
+      gsap.to(firstName.leon[i], {
+        duration,
+        size,
+        weight: 1,
+        ease: 'power4.inOut',
+      })
+      gsap.to(firstName.gap, {
+        duration,
+        x,
+        y,
+        ease: 'power4.inOut',
+        onComplete: function () {
+          animateFullScreen()
+        },
+      })
+    }
+
+    for (let i = 0; i < lastName.total; i++) {
+      gsap.to(lastName.gap, {
+        duration,
+        y: screenHeight,
+        ease: 'power4.inOut',
+      })
+    }
+  }
+
+  const animateFullScreen = () => {
+    const duration = 0.7
+    const size =
+      20 * Math.sqrt(screenWidth * screenWidth + screenHeight * screenHeight)
+    const x = -(size >> 1)
+    const y = size / 10
+
+    for (const l of firstName.leon) {
+      gsap.to(l, {
+        duration: duration,
+        weight: 900,
+        ease: 'power4.inOut',
+      })
+      gsap.to(l, {
+        duration: duration,
+        size,
+        ease: 'power4.in',
+      })
+    }
+    gsap.to(firstName.gap, {
+      duration: duration,
+      x,
+      y,
+      ease: 'power4.in',
+      onComplete: function () {
+        // Router.push('/home')
+      },
+    })
   }
 
   useEffect(() => {
     init()
-    requestAnimationFrame(draw)
-    // animateStroke()
+    requestAnimationFrame(render)
+    animateZoom()
   }, [canvas])
 
   useEffect(() => {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from 'next/head'
 import Leon from '../components/leon/Leon'
 
-export const Home = (): JSX.Element => (
+export const Intro = (): JSX.Element => (
   <div className="container">
     <Head>
       <title>Create Next App</title>
       <link rel="icon" href="/favicon.ico" />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/dat-gui/0.7.6/dat.gui.js"></script>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+      />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     </Head>
 
     <main>
@@ -33,11 +35,10 @@ export const Home = (): JSX.Element => (
       }
 
       canvas {
-        border: 1px solid black;
         display: block;
       }
     `}</style>
   </div>
 )
 
-export default Home
+export default Intro
